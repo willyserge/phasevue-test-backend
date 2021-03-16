22f14ded134ffa44b5dc0c78e58895fd1efd99de
willyserge <willyserge01@gmail.com>
2021-03-16 16:27:06 +0200

    fix eslint issue

--- src/controllers/deliverableCtrl.js
+++ src/controllers/deliverableCtrl.js
@@ -5,7 +5,9 @@ import Phase from '../models/phase';
 const Deliverables = {
 
   async getPhaseDeliverables(req, res) {
-    const deliverables = await Phase.find({ _id: req.body.id }).populate('deliverables').sort({ createdAt: -1 });
+    const deliverables = await Phase.find(
+      { _id: req.body.id }
+    ).populate('deliverables').sort({ createdAt: -1 });
     res.status(200).json(deliverables);
   },
 
