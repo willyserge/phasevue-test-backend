7420f82b411471377dbe73c415e4f808255a7014
willyserge <willyserge01@gmail.com>
2021-05-01 19:51:18 +0200

    cookie domain

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,6 +35,7 @@ const Auth = {
     const accessToken = createAccessToken({ id: user._id, email: user.email, name: user.name });
     const options = {
       expires: new Date(Date.now() + maxAge),
+      domain: 'app.phasevue.com',
       secure: NODE_ENV === 'prodution',
       httpOnly: NODE_ENV === 'production'
     };
